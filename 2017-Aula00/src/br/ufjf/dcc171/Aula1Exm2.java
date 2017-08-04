bd28016847c88264f3c18970402205c12dfbc439
João <>
2017-08-03 21:44:36 -0300

    Início

---
+++ 2017-Aula00/src/br/ufjf/dcc171/Aula1Exm2.java
@@ -0,0 +1,10 @@
+package br.ufjf.dcc171;
+
+
+public class Aula1Exm2 {
+    public static void main(String[] args) {
+        String nome = "Olaf";
+        System.out.println("Olá, eu sou " +nome+ " e gosto de abraços quentinhos");
+        
+    }
+}
