c98c54278fa3ce1211c779d447526d4ab7883a15
João <jpdias1997@hotmail.com>
2017-08-08 22:38:10 -0300

    Inserção de variáveis

--- Exercicio01/src/exercicio01/Exercicio01.java
+++ Exercicio01/src/exercicio01/Exercicio01.java
@@ -1,12 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exercicio01;
-public class Exercicio01{
+
+import javax.swing.JOptionPane;
+
+public class Exercicio01 {
 
     public static void main(String[] args) {
-    }
+        while (true) {
+            try {
+                Float largura = Float.parseFloat(JOptionPane.showInputDialog(null, "Digite a Largura"));
+                Float comprimento = Float.parseFloat(JOptionPane.showInputDialog(null, "Digite o Comprimento"));
+                Float altura = Float.parseFloat(JOptionPane.showInputDialog(null, "Digite a Altura"));
+                JOptionPane.showMessageDialog(null, largura,
+                        "Resultado",
+                        JOptionPane.INFORMATION_MESSAGE);
+                break;
 
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "Digite um número inteiro para as duas opções", "Entrada Inválida", JOptionPane.ERROR_MESSAGE);
+
+            }
+        }
+    }
 }
