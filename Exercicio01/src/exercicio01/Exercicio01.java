51c3bb2d17116be6fcd051d702c42cdc32a1418e
João Paulo Dias <jpdias1997@hotmail.com>
2017-08-09 22:37:24 -0300

    Cadastro de Produtos

--- Exercicio01/src/exercicio01/Exercicio01.java
+++ Exercicio01/src/exercicio01/Exercicio01.java
@@ -5,23 +5,21 @@ import javax.swing.JOptionPane;
 public class Exercicio01 {
 
     public static void main(String[] args) {
-        Float largura, comprimento,altura;
+        Float largura, comprimento, altura;
         while (true) {
             try {
                 largura = Float.parseFloat(JOptionPane.showInputDialog(null, "Digite a Largura"));
                 comprimento = Float.parseFloat(JOptionPane.showInputDialog(null, "Digite o Comprimento"));
                 altura = Float.parseFloat(JOptionPane.showInputDialog(null, "Digite a Altura"));
-                JOptionPane.showMessageDialog(null, "O volume do pacote é:" + largura*comprimento*altura,
+                JOptionPane.showMessageDialog(null, "O volume do pacote é:" + largura * comprimento * altura,
                         "Resultado",
                         JOptionPane.INFORMATION_MESSAGE);
                 break;
-
             } catch (NumberFormatException ex) {
                 JOptionPane.showMessageDialog(null, "Digite os valores utilizando números inteiros ou decimais", "Entrada Inválida", JOptionPane.ERROR_MESSAGE);
 
-            }
-            catch (NullPointerException ex) {
-               break;
+            } catch (NullPointerException ex) {
+                break;
             }
         }
     }
