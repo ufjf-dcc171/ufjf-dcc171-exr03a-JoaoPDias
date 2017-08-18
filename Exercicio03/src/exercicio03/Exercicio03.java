210867468b90eb106e372670605af0fdaa5dce22
ice <ice@lab06m30.nrc.ice.ufjf.br>
2017-08-17 22:55:06 -0300

    Exercício 03

---
+++ Exercicio03/src/exercicio03/Exercicio03.java
@@ -0,0 +1,14 @@
+package exercicio03;
+
+import javax.swing.JFrame;
+
+public class Exercicio03 {
+
+    public static void main(String[] args) {
+        JanelaCalculo janela = new JanelaCalculo();
+        janela.setSize(300,300);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setLocationRelativeTo(null);
+        janela.setVisible(true);
+    }
+}
